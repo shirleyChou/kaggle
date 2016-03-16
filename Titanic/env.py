b9465ac33e4a819d46cf5dddfaa08ee8fb8fc9bc
zhouyi <zhouyi@ibuka.cn>
2016-03-16 15:27:35 +0800

    add code

--- Titanic/env.py
+++ Titanic/env.py
@@ -3,6 +3,9 @@
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
-import matplotlib.pyplot as plt
+import pylab as plt
 import seaborn as sns
 
+
+# Set the global default size of matplotlib figures
+plt.rc('figure', figsize=(10, 5))   # (weight, high)
